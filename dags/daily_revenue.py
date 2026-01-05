d4152a0583284900e0fe094fe50c05b5a0bae632
abdallah-nasir <abdallah.nasir@ymail.com>
2026-01-05 11:09:19 +0200

    Enhance `daily_revenue` DAG: add revenue plotting, filter by date range, and calculate total revenue on specific date.

--- dags/daily_revenue.py
+++ dags/daily_revenue.py
@@ -1,12 +1,15 @@
+from datetime import timedelta
+
+import pandas as pd
 from airflow import DAG
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta
-from airflow.hooks.postgres_hook import PostgresHook
-import pandas as pd
+from matplotlib import pyplot as plt
 
 PG_CONN_ID = "pg_default"
 
+# make the current project folder path
 default_args = {
     "owner": "admin",
     "retries": 1,
@@ -27,10 +30,12 @@ def get_conn():
 
 
 READ_DATA_SQL = """
-    select o.orderdate::date, od.productid, p.productname, od.quantity, p.price
-    from orders o
-    join order_details od on o.orderid = od.orderid
-    join products p on od.productid = p.productid
+select o.orderdate::date, od.productid, p.productname, od.quantity, p.price
+from orders o
+join order_details od on o.orderid = od.orderid
+join products p on od.productid = p.productid
+--  filter with year 1996 only
+where o.orderdate::date between '1996-08-01' and '1996-08-31'
 """
 # task1
 def read_data():
@@ -40,7 +45,6 @@ def read_data():
     data = cursor.fetchall()
     cursor.close()
     conn.close()
-    print("data", data)
     return data
 
 # task2
@@ -54,7 +58,22 @@ def calculate_daily_revenue():
     data["revenue"] = data["quantity"] * data["price"]
     daily_revenue = data.groupby("orderdate")["revenue"].sum().reset_index()
     daily_revenue.to_csv("/home/airflow/daily_revenue.csv", index=False)
-    return daily_revenue.to_dict("records")
+    return data
+
+# task 4 export revenue plot to png
+def plot_daily_revenue(data: list):
+    data = pd.DataFrame(data, columns=["orderdate", "revenue"])
+    data["orderdate"] = pd.to_datetime(data["orderdate"]).dt.date
+    data.plot(x="orderdate", y="revenue", kind="line")
+    plt.savefig("/home/airflow/daily_revenue.png")
+    plt.show()
+
+# total revenue on 1996-08-08
+def calculate_total_revenue_on_date(date: str="1996-08-08"):
+    data = pd.read_csv("/home/airflow/daily_revenue.csv")
+    total_revenue = data[data["orderdate"] == date]["revenue"].sum()
+    return total_revenue
+
 
 with DAG(
     dag_id="daily_revenue",
@@ -79,6 +98,15 @@ with DAG(
         task_id="calculate_daily_revenue",
         python_callable=calculate_daily_revenue,
     )
+    t4 = PythonOperator(
+        task_id="plot_daily_revenue",
+        python_callable=plot_daily_revenue,
+        op_kwargs={"data": t3.output},
+    )
+    t5 = PythonOperator(
+        task_id="calculate_total_revenue_on_date",
+        python_callable=calculate_total_revenue_on_date,
+    )
 
-    t1 >> t2 >> t3
+    t1 >> t2 >> t3 >> t4 >> t5
 
